8fa6077dd730d30ab70f75f6ff9c19629d272655
Rederz <lurendel@gmail.com>
2023-01-17 16:42:57 -0600

    se agrega funcionalidad para la actividad inicial SplashScreen.kt

--- app/src/main/java/com/test/currencyconverter/SplashScreen.kt
+++ app/src/main/java/com/test/currencyconverter/SplashScreen.kt
@@ -1,11 +1,35 @@
 package com.test.currencyconverter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreen : AppCompatActivity() {
+    private var mDelayHandler: Handler? = null
+    private val splashDelay: Long = 3000
+
+    private val mRunnable: Runnable = Runnable {
+
+        val homeIntent = Intent(this, HomePage::class.java)
+        finish()
+        startActivity(homeIntent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        mDelayHandler = Handler()
+        mDelayHandler!!.postDelayed(mRunnable, splashDelay)
     }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
 }
