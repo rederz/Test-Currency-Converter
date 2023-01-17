d43983f2396b3cf4eae7db1587d50f107f4a5348
Rederz <lurendel@gmail.com>
2023-01-17 14:33:02 -0600

    se agrega navigation_pane, y se renombra activity_main con activity_home_page y la clase MainActivity con HomePage

--- app/src/main/java/com/test/currencyconverter/HomePage.kt
+++ app/src/main/java/com/test/currencyconverter/HomePage.kt
@@ -3,9 +3,9 @@ package com.test.currencyconverter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_page)
     }
 }
